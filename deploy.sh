e1e7f12d56cf72862c8a80d9be8d3c572f17ff6d
Marlon2025c <bastien.lecour2025@gmail.com>
2024-09-06 04:20:19 +0200

    ✅

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Chemin du projet
 PROJECT_DIR="/var/www/html/app"
-
-# Chemin du fichier de log
 LOG_FILE="/var/log/deploy.log"
-
-# Date actuelle pour le log
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-# Se déplacer dans le répertoire du projet
 cd $PROJECT_DIR
 
-# Mettre à jour le projet et enregistrer les résultats dans le fichier de log
-notify-send "[$DATE] Début de la mise à jour du projet..." >> $LOG_FILE
-git pull origin main >> $LOG_FILE 2>&1
+# Vérifie si des mises à jour sont disponibles avant de faire git pull
+UPDATES=$(git fetch --dry-run 2>&1)
 
-# Enregistrer le statut de la mise à jour
-if [ $? -eq 0 ]; then
-    notify-send "[$DATE] Mise à jour réussie." >> $LOG_FILE
+if [ -z "$UPDATES" ]; then
+    notify-send "${DATE} Pas de nouvelles mises à jour disponibles." >> $LOG_FILE
 else
-    notify-send "[$DATE] Échec de la mise à jour." >> $LOG_FILE
+    notify-send "${DATE} Mise à jour disponible. Démarrage de git pull..." >> $LOG_FILE
+    git pull origin main >> $LOG_FILE 2>&1
+    
+    if [ $? -eq 0 ]; then
+        notify-send "${DATE} Mise à jour réussie." >> $LOG_FILE
+    else
+        notify-send "${DATE} Échec de la mise à jour." >> $LOG_FILE
+    fi
 fi
