51887630e6c0777b3d95bd9f12a5fc7923fd86d3
Marlon2025c <bastien.lecour2025@gmail>
2024-10-10 21:36:48 +0200

    🚧 BDD 🚧

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'http://192.168.1.177',
+        'username'     => 'root',
+        'password'     => 'Spazia2025!!',
+        'database'     => 'Spazia',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
