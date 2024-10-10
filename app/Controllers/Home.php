f330a6cad9695e8e8138b8d6c93970893046e711
Marlon2025c <bastien.lecour2025@gmail>
2024-10-10 21:39:51 +0200

    test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
     public function index(): string
     {
+        $db = \Config\Database::connect();
+        if ($db->connID) {
+            return 'Connexion réussie à la base de données !';
+        } else {
+            return 'Échec de la connexion à la base de données.';
+        }
         return view('login');
     }
 
