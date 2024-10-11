b44be25deb339eb692171c2693200d0b0bf849db
Marlon2025c <bastien.lecour2025@gmail>
2024-10-11 19:02:07 +0200

    🚧

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,23 +8,9 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        try {
-            $db = \Config\Database::connect();
-
-            // Vérifie si la connexion a réussi
-            if ($db->connID) {
-                return view('login');
-            } else {
-                // Affiche une erreur personnalisée
-                return 'Échec de la connexion à la base de données. Vérifiez les paramètres de connexion.';
-            }
-        } catch (DatabaseException $e) {
-            // Affiche les détails de l'erreur
-            return 'Une erreur est survenue : ' . $e->getMessage();
-        } catch (\Exception $e) {
-            // Gère d'autres exceptions
-            return 'Une erreur inattendue est survenue : ' . $e->getMessage();
-        }
+        $db = \Config\Database::connect();
+        $db->connID;
+        return view('login');
     }
 
     public function welcome(): string
