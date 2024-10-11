73d5e6af4b03f81fc5f49d4a2e85c2fc45210f3e
Marlon2025c <bastien.lecour2025@gmail>
2024-10-11 11:10:02 +0200

    🚑

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,7 +26,7 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '192.168.1.177',
+        'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => 'Spazia2025!!',
         'database'     => 'Spazia',
