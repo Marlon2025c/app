6aafe467f83b20b2bac106d103b79978042520d4
Marlon2025c <bastien.lecour2025@gmail>
2024-10-12 08:35:20 +0200

    🚧

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,48 +2,19 @@
 
 namespace App\Controllers;
 
-use App\Models\UserModel;
+use CodeIgniter\Database\Exceptions\DatabaseException;
 
 class Home extends BaseController
-{
+    {
     public function index(): string
-    {
-        $userModel = new UserModel();
+        {
+            $db = \Config\Database::connect();
+            $db->connID;
+            return view('login');
+        }
 
-        // Récupérer tous les utilisateurs
-        $data['users'] = $userModel->findAll();
-
-        // Charger une vue avec les données récupérées
-        return view('users/index', $data);
-    }
-
-    public function getUser($id): string
-    {
-        $userModel = new UserModel();
-
-        // Récupérer un utilisateur spécifique par son ID
-        $data['user'] = $userModel->find($id);
-        // Charger une vue ou traiter les données
-        return view('users/detail', $data);
-    }
-}
-
-
-##  namespace App\Controllers;
-
-## use CodeIgniter\Database\Exceptions\DatabaseException;
-
-## class Home extends BaseController
-## {
-   ##  public function index(): string
-    ## {
-       ##  $db = \Config\Database::connect();
-        ## $db->connID;
-        ## return view('login');
-    ## }
-
-    ## public function welcome(): string
-    ## {
-        ## return view('welcome_message');
-    ## }
-## }
+    public function welcome(): string
+        {
+            return view('welcome_message');
+        }
+     }
