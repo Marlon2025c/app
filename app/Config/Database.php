3c4700ea35edc33e1a6311018b2e9517978df4ff
Marlon2025c <bastien.lecour2025@gmail>
2024-10-13 23:20:14 +0200

    🚧

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -30,7 +30,7 @@ class Database extends Config
         'username'     => 'marlon2025c',
         'password'     => 'Spazia2025!!',
         'database'     => 'Spazia',
-        'DBDriver'     => 'MySQL',
+        'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
