74b67e44db9fb7a1f31107ba2cf3f092f92ff158
Marlon2025c <bastien.lecour2025@gmail>
2024-10-14 00:17:05 +0200

    🚧

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,14 +7,20 @@ class Home extends BaseController
     public function index(): string
         {
             
-            $mysqli = new \mysqli('192.168.1.177', 'marlon2025c', 'Spazia2025!!', 'Spazia');
+            // $mysqli = new \mysqli('192.168.1.177', 'marlon2025c', 'Spazia2025!!', 'Spazia');
 
-            if ($mysqli->connect_error) {
-                die('Erreur de connexion MySQLi : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+            // if ($mysqli->connect_error) {
+            //     die('Erreur de connexion MySQLi : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+            // }
+
+            // echo 'Connexion réussie avec MySQLi dans CodeIgniter';
+            $db = \Config\Database::connect();
+            if ($db->connID) {
+                echo 'Connexion réussie via Config\Database::connect()';
+                return view('login');
+            } else {
+                echo 'Erreur lors de la connexion';
             }
-
-            echo 'Connexion réussie avec MySQLi dans CodeIgniter';
-            return view('login');
         }
 
     public function welcome(): string
--- app/Views/users/index.php
+++
@@ -1,6 +0,0 @@
-<h2>Liste des utilisateurs</h2>
-<ul>
-    <?php foreach($users as $user): ?>
-        <li><?= esc($user['username']) ?> - <?= esc($user['email']) ?></li>
-    <?php endforeach; ?>
-</ul>
