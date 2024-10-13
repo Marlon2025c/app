1d5590f3f4513a8d58298740b4bf2d2330182b0f
Marlon2025c <bastien.lecour2025@gmail>
2024-10-13 18:22:21 +0200

    test bdd

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,8 +7,12 @@ class Home extends BaseController
     public function index(): string
         {
             $db = \Config\Database::connect();
-            $db->connID;
-            print $db;
+            if ($db->connID) {
+                echo "Connexion réussie !";
+            } else {
+                echo "Connexion échouée !";
+            }
+
             return view('login');
         }
 
