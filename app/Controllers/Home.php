6342a76597b01716db309bccbd5893ef5fa74be8
Marlon2025c <bastien.lecour2025@gmail.com>
2024-10-14 15:30:19 +0200

    🔒️

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,29 +3,28 @@
 namespace App\Controllers;
 
 class Home extends BaseController
-    {
+{
     public function index(): string
-        {
-            
-            // $mysqli = new \mysqli('192.168.1.177', 'marlon2025c', 'Spazia2025!!', 'Spazia');
+    {
 
-            // if ($mysqli->connect_error) {
-            //     die('Erreur de connexion MySQLi : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-            // }
+        // $mysqli = new \mysqli('192.168.1.177', 'marlon2025c', 'Spazia2025!!', 'Spazia');
 
-            // echo 'Connexion réussie avec MySQLi dans CodeIgniter';
-            $db = \Config\Database::connect();
-            if ($db->connID) {
-                echo 'Connexion réussie via Config\Database::connect()';
-            } else {
-                $error = $db->error();
-                echo 'Erreur lors de la connexion: ' . $error['message'] ?? 'Erreur inconnue';
-            }
-            return view('login');
+        // if ($mysqli->connect_error) {
+        //     die('Erreur de connexion MySQLi : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+        // }
+
+        // echo 'Connexion réussie avec MySQLi dans CodeIgniter';
+        $db = \Config\Database::connect();
+        if ($db->connID) {
+            echo 'Connexion réussie via Config\Database::connect()';
+        } else {
+            echo 'Erreur lors de la connexion: ';
         }
+        return view('login');
+    }
 
     public function welcome(): string
-        {
-            return view('welcome_message');
-        }
-     }
+    {
+        return view('welcome_message');
+    }
+}
